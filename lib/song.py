cf958a052e43cb63056a8c0fa9516d81077fd7e0
ndurushima <ndu1994@gmail.com>
2025-08-06 08:25:01 -0500

    added class methods

--- lib/song.py
+++ lib/song.py
@@ -2,6 +2,8 @@ class Song:
     song_count = 0
     genre_count = {}
     artists_count = {}
+    genres = []
+    artists =[]
 
     def __init__(self, name, artist, genre):
         self.name = name
@@ -25,7 +27,28 @@ class Song:
     @classmethod
     def artists(cls):
         return list(cls.artists_count.keys())
+    
+    @classmethod
+    def add_song_to_count(cls, song):
+        cls.song_count += 1
+    
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)    
+    
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+    
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
 
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        cls.artists_count[artist] = cls.artists_count.get(artist, 0) + 1
 
 s1 = Song("Halo", "Beyonce", "Pop")
 s2 = Song("Crazy in Love", "Beyonce", "R&B")
