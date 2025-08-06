bb73a0d7aad2e0454dcc2413ad61625ab287c418
ndurushima <ndu1994@gmail.com>
2025-08-06 08:41:03 -0500

    trying to fix smalll syntax issues

--- lib/song.py
+++ lib/song.py
@@ -1,24 +1,24 @@
 class Song:
-    song_count = 0
+    count = 0
     genre_count = {}
-    artists_count = {}
+    artist_count = {}
     genres = []
-    artists =[]
+    artists = []
 
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
 
-        Song.song_count += 1
+        Song.count += 1
 
         Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
 
-        Song.artists_count[artist] = Song.artists_count.get(artist, 0) + 1
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
 
     @classmethod
     def count(cls):
-        return cls.song_count
+        return cls.count
 
     @classmethod
     def genres(cls):
@@ -26,11 +26,11 @@ class Song:
 
     @classmethod
     def artists(cls):
-        return list(cls.artists_count.keys())
+        return list(cls.artist_count.keys())
     
     @classmethod
-    def add_song_to_count(cls, song):
-        cls.song_count += 1
+    def add_song_to_count(cls):
+        cls.count += 1
     
     @classmethod
     def add_to_genres(cls, genre):
@@ -48,7 +48,7 @@ class Song:
 
     @classmethod
     def add_to_artist_count(cls, artist):
-        cls.artists_count[artist] = cls.artists_count.get(artist, 0) + 1
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
 
 s1 = Song("Halo", "Beyonce", "Pop")
 s2 = Song("Crazy in Love", "Beyonce", "R&B")
