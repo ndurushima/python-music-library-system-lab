79f93196c7ac7dfda1a622a8326c808b860d6c4d
ndurushima <ndu1994@gmail.com>
2025-08-06 11:03:24 -0500

    "fixed init and made it self.songcount"

--- lib/song.py
+++ lib/song.py
@@ -9,8 +9,13 @@ class Song:
         self.name = name
         self.artist = artist
         self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
 
-        Song.song_count += 1
+        self.song_count += 1
 
         Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
 
@@ -29,7 +34,7 @@ class Song:
         return list(cls.artist_count.keys())
     
     @classmethod
-    def add_song_to_count(cls, song):
+    def add_song_to_count(cls):
         cls.song_count += 1
     
     @classmethod
@@ -44,11 +49,17 @@ class Song:
     
     @classmethod
     def add_to_genre_count(cls, genre):
-        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+        if cls.genre_count[genre]:
+            cls.genre_count.get(genre, 0) + 1
+        else:
+            cls.genre_count[genre] = 1
 
     @classmethod
     def add_to_artist_count(cls, artist):
-        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
+        if cls.artist_count[artist]: 
+            cls.artist_count.get(artist, 0) + 1
+        else:
+            cls.artist_count[artist] = 1
 
 s1 = Song("Halo", "Beyonce", "Pop")
 s2 = Song("Crazy in Love", "Beyonce", "R&B")
