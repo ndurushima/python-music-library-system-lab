0410f13e91876fff9ecc998ac821d87a5ed68ff6
ndurushima <ndu1994@gmail.com>
2025-08-06 08:31:54 -0500

    trying a different wat to pass the tests

--- lib/song.py
+++ lib/song.py
@@ -10,50 +10,66 @@ class Song:
         self.artist = artist
         self.genre = genre
 
-        Song.song_count += 1
+        Song.count += 1
 
+        # 2) unique genres list
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        # 3) unique artists list
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        # 4) per‐genre tally
         Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
 
-        Song.artists_count[artist] = Song.artists_count.get(artist, 0) + 1
+        # 5) per‐artist tally
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
 
-    @classmethod
-    def count(cls):
-        return cls.song_count
+        # Song.song_count += 1
 
-    @classmethod
-    def genres(cls):
-        return list(cls.genre_count.keys())
+        # Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
 
-    @classmethod
-    def artists(cls):
-        return list(cls.artists_count.keys())
+        # Song.artists_count[artist] = Song.artists_count.get(artist, 0) + 1
+
+#     @classmethod
+#     def count(cls):
+#         return cls.song_count
+
+#     @classmethod
+#     def genres(cls):
+#         return list(cls.genre_count.keys())
+
+#     @classmethod
+#     def artists(cls):
+#         return list(cls.artists_count.keys())
     
-    @classmethod
-    def add_song_to_count(cls, song):
-        cls.song_count += 1
+#     @classmethod
+#     def add_song_to_count(cls, song):
+#         cls.song_count += 1
     
-    @classmethod
-    def add_to_genres(cls, genre):
-        if genre not in cls.genres:
-            cls.genres.append(genre)    
+#     @classmethod
+#     def add_to_genres(cls, genre):
+#         if genre not in cls.genres:
+#             cls.genres.append(genre)    
     
-    @classmethod
-    def add_to_artists(cls, artist):
-        if artist not in cls.artists:
-            cls.artists.append(artist)
+#     @classmethod
+#     def add_to_artists(cls, artist):
+#         if artist not in cls.artists:
+#             cls.artists.append(artist)
     
-    @classmethod
-    def add_to_genre_count(cls, genre):
-        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+#     @classmethod
+#     def add_to_genre_count(cls, genre):
+#         cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
 
-    @classmethod
-    def add_to_artist_count(cls, artist):
-        cls.artists_count[artist] = cls.artists_count.get(artist, 0) + 1
+#     @classmethod
+#     def add_to_artist_count(cls, artist):
+#         cls.artists_count[artist] = cls.artists_count.get(artist, 0) + 1
 
-s1 = Song("Halo", "Beyonce", "Pop")
-s2 = Song("Crazy in Love", "Beyonce", "R&B")
+# s1 = Song("Halo", "Beyonce", "Pop")
+# s2 = Song("Crazy in Love", "Beyonce", "R&B")
 
-print(Song.count())  # Output: 2
-print(Song.genres())  # Output: ['Pop', 'R&B']
-print(Song.artists())  # Output: ['Beyonce']
-print(Song.genre_count)  # Output: {'Pop': 1, 'R&B': 1}
+# print(Song.count())  # Output: 2
+# print(Song.genres())  # Output: ['Pop', 'R&B']
+# print(Song.artists())  # Output: ['Beyonce']
+# print(Song.genre_count)  # Output: {'Pop': 1, 'R&B': 1}
